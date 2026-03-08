d10baf74bad407e26c6ae634fb605f8f829cfc36
abdiswamad <abdisamadmohamud@students.zetech.ac.ke>
2026-03-08 09:19:02 +0300

    Add files via upload

---
+++ 2DArray.c
@@ -0,0 +1,33 @@
+//2D Array
+//ABDISAMAD ALI
+//BCS-03-0001/2026
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int occupancy[5][10];
+    int i, j;
+    int occupied, vacant;
+
+    srand(1);
+
+    for (i = 0; i < 5; i++) {
+        occupied = 0;
+        vacant = 0;
+
+        for (j = 0; j < 10; j++) {
+            occupancy[i][j] = rand() % 2;
+
+            if (occupancy[i][j] == 1)
+                occupied++;
+            else
+                vacant++;
+        }
+
+        printf("Floor %d -> Occupied: %d, Vacant: %d\n",
+               i + 1, occupied, vacant);
+    }
+
+    return 0;
+}
