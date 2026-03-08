0421d903a92d3a28ab93be15ae4d465984740b7a
abdiswamad <abdisamadmohamud@students.zetech.ac.ke>
2026-03-08 09:24:00 +0300

    Add files via upload

---
+++ 3DArray.c
@@ -0,0 +1,29 @@
+//3D Array
+//ABDISAMAD ALI
+//BCS-03-0001/2026
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int chain[3][5][10];
+    int p, q, r;          
+    int totalOccupied = 0;
+
+    for (p = 0; p < 3; p++) {
+        for (q = 0; q < 5; q++) {
+            for (r = 0; r < 10; r++) {
+                chain[p][q][r] = rand() % 2;
+
+                if (chain[p][q][r] == 1) {
+                    totalOccupied++;
+                }
+            }
+        }
+    }
+
+    
+    printf("Total occupied rooms across all branches: %d\n", totalOccupied);
+
+    return 0;
+}
