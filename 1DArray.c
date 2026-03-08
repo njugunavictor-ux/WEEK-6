9e72d04db1eb73f2fc5835f3cc62e87e1102e74d
abdiswamad <abdisamadmohamud@students.zetech.ac.ke>
2026-03-08 09:16:47 +0300

    Add files via upload

---
+++ 1DArray.c
@@ -0,0 +1,34 @@
+//1D Array
+//ABDISAMAD ALI
+//BCS-03-0001/2026
+
+#include <stdio.h>
+
+#include <stdio.h>
+
+int main() {
+    float revenue[7];
+    float total = 0.0;
+    float average;
+    int i;   
+
+    
+    for (i = 0; i < 7; i++) {
+        printf("Enter revenue for day %d: ", i + 1);
+        scanf("%f", &revenue[i]);
+    }
+
+    
+    for (i = 0; i < 7; i++) {
+        total += revenue[i];
+    }
+
+    
+    average = total / 7;
+
+    
+    printf("\nTotal weekly revenue: %.2f\n", total);
+    printf("Average daily revenue: %.2f\n", average);
+
+    return 0;
+}
